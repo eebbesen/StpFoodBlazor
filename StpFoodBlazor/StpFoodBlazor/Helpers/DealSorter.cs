d6a10083fb2619bd3f66b6ba5b6612aac14b11cd
eebbesen <eebbesen.git@gmail.com>
2025-03-14 21:17:39 -0500

    refactoring

--- StpFoodBlazor/StpFoodBlazor/Helpers/DealSorter.cs
+++ StpFoodBlazor/StpFoodBlazor/Helpers/DealSorter.cs
@@ -5,10 +5,10 @@ namespace StpFoodBlazor.Helpers
     public static class DealSorter
     {
         public static DealEvent[] Sort(DealEvent[] deals){
-            return deals.OrderBy(deal =>
+            return [.. deals.OrderBy(deal =>
                 string.IsNullOrEmpty(deal.Day) ? DayOfWeek.Sunday : Enum.Parse(typeof(DayOfWeek), deal.Day)
             ).ThenBy(deal => deal.Name).
-              ThenBy(deal => deal.Deal).ToArray();
+              ThenBy(deal => deal.Deal)];
         }
     }
 }
--- StpFoodBlazor/StpFoodBlazor/Helpers/GiftCardSorter.cs
+++ StpFoodBlazor/StpFoodBlazor/Helpers/GiftCardSorter.cs
@@ -4,7 +4,8 @@ namespace StpFoodBlazor.Helpers
 {
     public static class GiftCardSorter
     {
-        public static GiftCard[] Sort(GiftCard[] giftcards){
+        public static GiftCard[] Sort(GiftCard[] giftcards)
+        {
             return [.. giftcards.OrderBy(giftcard => giftcard.Name)
                 .ThenBy(giftcard => giftcard.Deal)];
         }
