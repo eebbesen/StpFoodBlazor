fb132778082163c8aa2e12b8a83b9266e6a157db
eebbesen <eebbesen.git@gmail.com>
2025-04-17 13:38:35 -0500

    Test refactor

--- StpFoodBlazor/StpFoodBlazorTest/Integration/IntegrationTest.cs
+++ StpFoodBlazor/StpFoodBlazorTest/Integration/IntegrationTest.cs
@@ -2,6 +2,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.Extensions;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace StpFoodBlazorTest.Integration
 {
@@ -65,9 +66,28 @@ namespace StpFoodBlazorTest.Integration
             outputFile.WriteLine(Driver.PageSource);
         }
 
+// On Windows:
+// Error Message:
+//    System.FormatException : The input is not a valid Base-64 string as it contains a non-base 64 character, more than two padding characters, or an illegal character among the padding characters.
+//   Stack Trace:
+//      at System.Convert.FromBase64CharPtr(Char* inputPtr, Int32 inputLength)
+//    at System.Convert.FromBase64String(String s)
+//    at OpenQA.Selenium.EncodedFile..ctor(String base64EncodedFile)
+//    at OpenQA.Selenium.Screenshot..ctor(String base64EncodedScreenshot)
+//    at OpenQA.Selenium.WebDriver.GetScreenshot()
+//    at OpenQA.Selenium.Support.Extensions.WebDriverExtensions.TakeScreenshot(IWebDriver driver)
+//    at StpFoodBlazorTest.Integration.IntegrationTest.SeleniumScreenShot(String name) in C:\Users\User\source\repos\StpFoodBlazor\StpFoodBlazor\StpFoodBlazorTest\Integration\IntegrationTest.cs:line 59
+//    at StpFoodBlazorTest.Integration.IntegrationTest.SeleniumArtifacts(String name) in C:\Users\User\source\repos\StpFoodBlazor\StpFoodBlazor\StpFoodBlazorTest\Integration\IntegrationTest.cs:line 70
+//    at StpFoodBlazorTest.Integration.DealsTest.DealsTableBodyLoads() in C:\Users\User\source\repos\StpFoodBlazor\StpFoodBlazor\StpFoodBlazorTest\Integration\DealsTest.cs:line 61
+//    at System.RuntimeMethodHandle.InvokeMethod(Object target, Void** arguments, Signature sig, Boolean isConstructor)
+//    at System.Reflection.MethodBaseInvoker.InvokeWithNoArgs(Object obj, BindingFlags invokeAttr)
         protected void SeleniumArtifacts(String name)
         {
-            SeleniumScreenShot(name + ".png");
+            if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                SeleniumScreenShot(name + ".png");
+            }
+
             SeleniumSource(name + ".html");
         }
     }
