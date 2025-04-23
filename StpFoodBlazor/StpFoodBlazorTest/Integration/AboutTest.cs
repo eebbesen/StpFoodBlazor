8cf111935c1e3fe5e165292234b83abad4e03f72
eebbesen <eebbesen.git@gmail.com>
2025-04-23 15:41:37 -0500

    More about test fixing

--- StpFoodBlazor/StpFoodBlazorTest/Integration/AboutTest.cs
+++ StpFoodBlazor/StpFoodBlazorTest/Integration/AboutTest.cs
@@ -21,7 +21,7 @@ namespace StpFoodBlazorTest.Integration
             Assert.Equal(" flex-fill", Driver.FindElement(By.Id("root-nav")).GetAttribute("class"));
             Assert.Equal("Gift Cards", Driver.FindElement(By.Id("giftcard-nav")).Text);
             Assert.Equal(" justify-content-end", Driver.FindElement(By.Id("giftcard-nav")).GetAttribute("class"));
-            Assert.Equal("d-none", Driver.FindElement(By.Id("about-nav")).GetAttribute("class"));
+            Assert.Equal(" d-none", Driver.FindElement(By.Id("about-nav")).GetAttribute("class"));
             Assert.Contains("deals compiled by", Driver.FindElement(By.Id("about-content")).Text);
             Assert.Contains("version: ", Driver.FindElement(By.Id("about-content")).Text);
         }
