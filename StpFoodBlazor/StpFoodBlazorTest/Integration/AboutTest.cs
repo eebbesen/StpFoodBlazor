69dc29594523a2ff7d2d6a8ec15c8dc3c772974d
eebbesen <eebbesen.git@gmail.com>
2025-04-23 15:02:03 -0500

    fixing test issue

--- StpFoodBlazor/StpFoodBlazorTest/Integration/AboutTest.cs
+++ StpFoodBlazor/StpFoodBlazorTest/Integration/AboutTest.cs
@@ -18,9 +18,9 @@ namespace StpFoodBlazorTest.Integration
         private void AssertCommon()
         {
             Assert.Equal("Deals", Driver.FindElement(By.Id("root-nav")).Text);
-            Assert.Equal("flex-fill", Driver.FindElement(By.Id("root-nav")).GetAttribute("class"));
+            Assert.Equal(" flex-fill", Driver.FindElement(By.Id("root-nav")).GetAttribute("class"));
             Assert.Equal("Gift Cards", Driver.FindElement(By.Id("giftcard-nav")).Text);
-            Assert.Equal("justify-content-end", Driver.FindElement(By.Id("giftcard-nav")).GetAttribute("class"));
+            Assert.Equal(" justify-content-end", Driver.FindElement(By.Id("giftcard-nav")).GetAttribute("class"));
             Assert.Equal("d-none", Driver.FindElement(By.Id("about-nav")).GetAttribute("class"));
             Assert.Contains("deals compiled by", Driver.FindElement(By.Id("about-content")).Text);
             Assert.Contains("version: ", Driver.FindElement(By.Id("about-content")).Text);
