f150a56d3f91b4e65ab8ae197b8cc0b4d59a0254
eebbesen <eebbesen.git@gmail.com>
2025-04-24 15:22:51 -0500

    formatting

--- StpFoodBlazor/StpFoodBlazor/Services/HttpGiftCardService.cs
+++ StpFoodBlazor/StpFoodBlazor/Services/HttpGiftCardService.cs
@@ -4,7 +4,7 @@ using StpFoodBlazor.Models;
 
 namespace StpFoodBlazor.Services
 {
-    public class HttpGiftCardService(IMemoryCache memoryCache,HttpClient httpClient, ILogger<HttpGiftCardService> logger) : IGiftCardService
+    public class HttpGiftCardService(IMemoryCache memoryCache, HttpClient httpClient, ILogger<HttpGiftCardService> logger) : IGiftCardService
     {
         private static readonly string Url = Helper.GetUrl("giftcards");
         private readonly IMemoryCache _memoryCache = memoryCache;
