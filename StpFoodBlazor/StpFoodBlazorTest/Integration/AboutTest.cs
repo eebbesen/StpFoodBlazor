901fe6acb18037b99e82c4648da6c1f7c39e6b0c
eebbesen <eebbesen.git@gmail.com>
2025-05-02 13:08:09 -0500

    Adding notification dot for GiftCards link in navbar

--- StpFoodBlazor/StpFoodBlazorTest/Integration/AboutTest.cs
+++ StpFoodBlazor/StpFoodBlazorTest/Integration/AboutTest.cs
@@ -20,7 +20,7 @@ namespace StpFoodBlazorTest.Integration
             Assert.Equal("Deals", Driver.FindElement(By.Id("root-nav")).Text);
             Assert.Equal(" flex-fill", Driver.FindElement(By.Id("root-nav")).GetAttribute("class"));
             Assert.Equal("Gift Cards", Driver.FindElement(By.Id("giftcard-nav")).Text);
-            Assert.Equal(" justify-content-end", Driver.FindElement(By.Id("giftcard-nav")).GetAttribute("class"));
+            Assert.Equal(" justify-content-end position-relative", Driver.FindElement(By.Id("giftcard-nav")).GetAttribute("class"));
             Assert.Equal(" d-none", Driver.FindElement(By.Id("about-nav")).GetAttribute("class"));
             Assert.Contains("deals compiled by", Driver.FindElement(By.Id("about-content")).Text);
             Assert.Contains("version: ", Driver.FindElement(By.Id("about-content")).Text);
@@ -30,9 +30,9 @@ namespace StpFoodBlazorTest.Integration
         public void About()
         {
             try
-            {   
+            {
                 Driver.Navigate().GoToUrl(BaseUrl());
-                
+
                 AssertCommon();
             }
             catch (Exception ex)
