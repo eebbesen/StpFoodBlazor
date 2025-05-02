71a52ea0f17a23f8ace11a4cf3f8eb514a3cb095
eebbesen <eebbesen.git@gmail.com>
2025-05-02 15:10:54 -0500

    remove debug log

--- StpFoodBlazor/StpFoodBlazor/Services/HttpGiftCardService.cs
+++ StpFoodBlazor/StpFoodBlazor/Services/HttpGiftCardService.cs
@@ -7,23 +7,23 @@ namespace StpFoodBlazor.Services
     public class HttpGiftCardService(IMemoryCache memoryCache, HttpClient httpClient, ILogger<HttpGiftCardService> logger) : IGiftCardService
     {
         private static readonly string Url = Helper.GetUrl("giftcards");
-        private readonly IMemoryCache _memoryCache = memoryCache;
+        private readonly IMemoryCache _cache = memoryCache;
         private readonly ILogger<HttpGiftCardService> _logger = logger;
         private static readonly string CACHE_KEY = "giftcards";
 
         public async Task<GiftCard[]> GetGiftCardsAsync()
         {
             GiftCard[]? result;
-            
-            if (memoryCache.TryGetValue(CACHE_KEY, out GiftCard[]? cachedGiftcards))
+
+            if (_cache.TryGetValue(CACHE_KEY, out GiftCard[]? cachedGiftcards))
             {
                 result = cachedGiftcards;
                 _logger.LogInformation("retrieved deals from cache using key: {CacheKey}", CACHE_KEY);
-            } 
+            }
             else
             {
                 result = await httpClient.GetFromJsonAsync<GiftCard[]>(Url);
-                _memoryCache.Set(CACHE_KEY, result, new MemoryCacheEntryOptions
+                _cache.Set(CACHE_KEY, result, new MemoryCacheEntryOptions
                 {
                     AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(400)
                 });
