840c50e0b957b855276646b3c3dc45b3cabcf3e3
eebbesen <eebbesen.git@gmail.com>
2025-06-21 10:25:55 -0500

    refactoring secrets

--- StpFoodBlazor/Program.cs
+++ StpFoodBlazor/Program.cs
@@ -1,4 +1,5 @@
 using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging.AzureAppServices;
 using StpFoodBlazor.Components;
@@ -12,18 +13,34 @@ builder.Configuration.AddEnvironmentVariables();
 if (builder.Environment.IsProduction())
 {
     // In production, use managed identity to access Key Vault
-    var keyVaultEndpoint = new Uri($"https://feastival.vault.azure.net/");
-    builder.Configuration.AddAzureKeyVault(
-        keyVaultEndpoint,
-        new DefaultAzureCredential());
+    var keyVaultUrl = new Uri("https://feastival.vault.azure.net/");
+    var credential = new DefaultAzureCredential();
+    var secretClient = new SecretClient(keyVaultUrl, credential);
 
-    builder.Logging.AddApplicationInsights(
+    try
+    {
+        KeyVaultSecret secret = await secretClient.GetSecretAsync("AppInsightsConnectionString");
+        string connectionString = secret.Value;
+
+        builder.Logging.AddApplicationInsights(
         configureTelemetryConfiguration: (config) =>
-            config.ConnectionString = builder.Configuration["AppInsights"],
+            config.ConnectionString = connectionString,
         configureApplicationInsightsLoggerOptions: (options) => {
             options.IncludeScopes = true;
             options.TrackExceptionsAsExceptionTelemetry = true;
         });
+    }
+    catch (Exception ex)
+    {
+        // Log the exception, but don't expose sensitive details
+        builder.Logging.AddConsole();
+        var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger("Program");
+        logger.LogError(ex, "Failed to retrieve Application Insights connection string from Key Vault");
+        throw;
+    }
+
+
+
 }
 else
 {
